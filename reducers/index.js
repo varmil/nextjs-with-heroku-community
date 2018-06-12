943503fe38f5b51dcf601babf56f383f680b4452
Your Name <fkmks247@gmail.com>
2018-06-12 15:28:05 +0900

    add redux example

---
+++ reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux'
+import example from './example'
+// import auth from './auth'
+// import area from './area'
+// import searchForm from './searchForm'
+// import restaurants from './restaurants'
+// import restaurantDetail from './restaurantDetail'
+// import errors from './errors'
+// import loading from './loading'
+
+export const exampleInitialState = undefined
+
+const rootReducer = combineReducers({
+  example
+  // auth,
+  // area,
+  // searchForm,
+  // restaurants,
+  // restaurantDetail,
+  // errors,
+  // loading
+})
+
+export default rootReducer
