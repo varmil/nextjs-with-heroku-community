ad1442fe7e171754ae77e5a67ad7b6db5323b709
varmil <fkmks247@gmail.com>
2018-06-20 07:57:14 +0900

    backup ifles

--- components/templates/site/container/EditableNavBar.js
+++ components/templates/site/container/EditableNavBar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import BaseNavBar from 'components/templates/site/container/NavBar'
 import CommunityLogo from 'components/organisms/site/edit/CommunityLogo'
-import AccountIcon from 'components/organisms/site/edit/AccountIcon'
-import NotificationIcon from 'components/organisms/site/edit/NotificationIcon'
+// import AccountIcon from 'components/organisms/pc/AccountIcon'
+// import NotificationIcon from 'components/organisms/pc/NotificationIcon'
 
 // NOTE: extends component cause hot load broken !
 // export default class NavBar extends BaseNavBar {
@@ -19,8 +19,8 @@ const Index = props => (
   <React.Fragment>
     <BaseNavBar
       communityLogo={CommunityLogo}
-      accountIcon={AccountIcon}
-      notificationIcon={NotificationIcon}
+      // accountIcon={AccountIcon}
+      // notificationIcon={NotificationIcon}
     />
   </React.Fragment>
 )
