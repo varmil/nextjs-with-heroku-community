d92fbd10a90f3cfa6342d05a6b5ac8994961f2bc
varmil <fkmks247@gmail.com>
2018-06-21 08:12:58 +0900

    add react-select

--- components/templates/site/page/TalkRoomPage.js
+++ components/templates/site/page/TalkRoomPage.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import range from 'lodash/range'
 import Link from 'next/link'
+import Select from 'react-select'
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import { setMainBanner, setBoxHeader, setSubBanner } from 'actions/site'
 import BoxHeader from 'components/organisms/site/base/BoxHeader'
 import BoxContent from 'components/organisms/site/BoxContent'
 import Header from 'components/templates/site/container/Header'
 import dynamic from 'next/dynamic'
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+]
+
+const colourStyles = {
+  control: styles => ({
+    ...styles,
+    backgroundColor: 'white',
+    // border: 'none',
+    width: 190,
+    margin: '0 auto',
+    fontSize: 12
+  })
+}
 
 const initialState = {}
 let SubBanner
@@ -141,6 +159,17 @@ export default class TalkRoomPage extends React.Component {
                 <span>{talkRoom.inputForm.text}</span>
                 <i className="fas fa-image" />
               </div>
+            </Link>
+          </section>
+
+          <section className="cat container py-3 my-1">
+            <Link>
+              <Select
+                placeholder={'全カテゴリ'}
+                styles={colourStyles}
+                options={options}
+                isSearchable={false}
+              />
             </Link>
           </section>
 
