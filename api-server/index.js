9f389370246d5e053868bd8e094117b50516cffa
Your Name <fkmks247@gmail.com>
2018-06-22 20:01:01 +0900

    for GCP WIP

--- api-server/index.js
+++ api-server/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const path = require('path')
+// const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 // Priority serve any static files.
-app.use(
-  express.static(path.resolve(__dirname, '../react-web/.next/bundles/pages'))
-)
+// app.use(
+//   express.static(path.resolve(__dirname, '../react-web/.next/bundles/pages'))
+// )
 
 // Answer API requests.
 app.get('/api', function(req, res) {
@@ -17,11 +17,11 @@ app.get('/api', function(req, res) {
 })
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(
-    path.resolve(__dirname, '../react-web/.next/bundles/pages', 'index.html')
-  )
-})
+// app.get('*', function(request, response) {
+//   response.sendFile(
+//     path.resolve(__dirname, '../react-web/.next/bundles/pages', 'index.html')
+//   )
+// })
 
 app.listen(PORT, function() {
   console.error(`Node API Server ${process.pid}: listening on port ${PORT}`)
