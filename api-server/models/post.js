9640d05ad392b9c5652afbdbf8b042aa816b37a5
varmil <fkmks247@gmail.com>
2018-06-23 15:00:26 +0900

    use port 80 react-web on prod

--- api-server/models/post.js
+++ api-server/models/post.js
@@ -1,5 +1,6 @@
 module.exports = function(sequelize, DataTypes) {
   const Post = sequelize.define('Post', {
+    // クライアント側と定数を一致させる or サーバ側で文字列 - 数値変換
     boxType: DataTypes.INTEGER,
     posterId: DataTypes.INTEGER,
     isAdmin: DataTypes.BOOLEAN,
