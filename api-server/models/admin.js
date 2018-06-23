b019440efc5e1c54ab5c0ec6727c2a762e96ec57
varmil <fkmks247@gmail.com>
2018-06-23 10:00:58 +0900

    modify model definition for v4

--- api-server/models/admin.js
+++ api-server/models/admin.js
@@ -1,19 +1,8 @@
-'use strict'
 module.exports = function(sequelize, DataTypes) {
-  var Admin = sequelize.define(
-    'Admin',
-    {
-      first_name: DataTypes.STRING,
-      last_name: DataTypes.STRING,
-      email: DataTypes.STRING
-    },
-    {
-      classMethods: {
-        associate: function(models) {
-          // associations can be defined here
-        }
-      }
-    }
-  )
+  var Admin = sequelize.define('Admin', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING
+  })
   return Admin
 }
--- api-server/models/user.js
+++ api-server/models/user.js
@@ -1,30 +1,26 @@
-'use strict'
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define(
-    'User',
-    {
-      nickname: DataTypes.STRING,
-      facebook_id: DataTypes.BIGINT,
-      facebook_access_token: DataTypes.STRING,
-      first_name: DataTypes.STRING,
-      last_name: DataTypes.STRING,
-      profile_photo_path: DataTypes.STRING,
-      locale: DataTypes.STRING,
-      gender: DataTypes.STRING,
-      email: DataTypes.STRING,
-      age_range_min: DataTypes.INTEGER,
-      age_range_max: DataTypes.INTEGER,
-      timezone: DataTypes.FLOAT,
-      verified: DataTypes.BOOLEAN,
-      birthday: DataTypes.STRING
-    },
-    {
-      classMethods: {
-        associate: function(models) {
-          // associations can be defined here
-        }
-      }
-    }
-  )
+  const User = sequelize.define('User', {
+    nickname: DataTypes.STRING,
+    facebook_id: DataTypes.BIGINT,
+    facebook_access_token: DataTypes.STRING,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    profile_photo_path: DataTypes.STRING,
+    locale: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    email: DataTypes.STRING,
+    age_range_min: DataTypes.INTEGER,
+    age_range_max: DataTypes.INTEGER,
+    timezone: DataTypes.FLOAT,
+    verified: DataTypes.BOOLEAN,
+    birthday: DataTypes.STRING
+  })
+
+  // Class Method
+  User.associate = models => {}
+
+  // Instance Method
+  User.prototype.someMethod = () => {}
+
   return User
 }
