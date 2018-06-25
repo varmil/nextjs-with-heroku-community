ff68cbb77260cd8950ea42d907ca776dc527cfd1
Your Name <fkmks247@gmail.com>
2018-06-25 15:23:26 +0900

    fix bug, call getInitialProps correctly

--- react-web/components/templates/withAppLayout.js
+++ react-web/components/templates/withAppLayout.js
@@ -3,6 +3,16 @@ import LoadingHeader from 'components/organisms/LoadingHeader'
 
 export default function withAppLayout(ComposedComponent) {
   class AppLayout extends Component {
+    static async getInitialProps(props) {
+      let pageProps = {}
+
+      if (ComposedComponent.getInitialProps) {
+        pageProps = await ComposedComponent.getInitialProps(props)
+      }
+
+      return { ...pageProps }
+    }
+
     render() {
       return (
         <React.Fragment>
--- react-web/routes.js
+++ react-web/routes.js
@@ -1,5 +1,8 @@
 const nextRoutes = require('next-routes')
 const routes = (module.exports = nextRoutes())
 
-routes.add('blog', '/blog/:slug')
-routes.add('about', '/about-us/:foo(bar|baz)')
+// routes.add('blog', '/blog/:slug')
+// routes.add('/view/about', '/about-us/:foo(bar|baz)')
+
+// box page
+routes.add('/home/:slug', 'home')
