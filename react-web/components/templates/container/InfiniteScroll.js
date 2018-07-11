6566f6acdec8bd6c26b8579dccb35112404b0541
Your Name <fkmks247@gmail.com>
2018-07-11 15:38:44 +0900

    change threshold 6 to 130

--- react-web/components/templates/container/InfiniteScroll.js
+++ react-web/components/templates/container/InfiniteScroll.js
@@ -50,7 +50,7 @@ class InfiniteContents extends React.Component {
         pageStart={Math.ceil(this.props.length / PER_PAGE)}
         loadMore={this.loadMoreRows.bind(this)}
         hasMore={!disabled && !this.state.isLoading && this.state.hasMore}
-        threshold={6}
+        threshold={130}
         loader={
           <div className="loader" key={0}>
             Loading ...
