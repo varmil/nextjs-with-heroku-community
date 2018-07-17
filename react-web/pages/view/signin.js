f07b2c18c6f822562311bc8dac0e8240c19805ee
Your Name <fkmks247@gmail.com>
2018-07-17 19:06:55 +0900

    modify /view/signin

--- react-web/pages/view/signin.js
+++ react-web/pages/view/signin.js
@@ -4,18 +4,19 @@ import { createAction } from 'redux-actions'
 import { User } from 'constants/ActionTypes'
 import { Link, Router } from 'routes'
 import Input from 'reactstrap/lib/Input'
-import RoundWideButton from 'components/atoms/RoundWideButton'
 import ColorButton from 'components/atoms/ColorButton'
 import CenteredContainer from 'components/molecules/CenteredContainer'
 import SignInUpHeader from 'components/molecules/SignInUpHeader'
+import Rule from 'constants/Rule'
 
-const btnStyle = {
-  width: '100%'
-}
-const inputStyle = {
-  borderRadius: 30,
-  padding: '10px 20px'
-}
+// import RoundWideButton from 'components/atoms/RoundWideButton'
+// const btnStyle = {
+//   width: '100%'
+// }
+// const inputStyle = {
+//   borderRadius: 15,
+//   padding: '10px 20px'
+// }
 
 const E_MESSAGE =
   'メールアドレスかパスワードが間違っています。再度入力し直してください。'
@@ -49,14 +50,16 @@ class Signin extends React.Component {
   }
 
   render() {
+    const { email, password } = this.state
+
     return (
       <React.Fragment>
-        <CenteredContainer height={520}>
+        <CenteredContainer height={430}>
           <section>
             <SignInUpHeader text="ログイン" route={'/view/signup'} />
           </section>
 
-          <section className="mt-5">
+          {/* <section className="oauth mt-5">
             <RoundWideButton
               className="my-2"
               style={btnStyle}
@@ -77,31 +80,41 @@ class Signin extends React.Component {
 
           <section className="my-4">
             <img src="/static/img/login-or-big.png" />
-          </section>
+          </section> */}
 
-          <section className="">
+          <section className="mt-5">
             <div className="form-group">
+              <label>メールアドレス</label>
               <Input
                 type="email"
-                style={inputStyle}
-                placeholder="メールアドレス"
+                // style={inputStyle}
+                placeholder="メールアドレスを入力"
                 value={this.state.email}
                 onChange={this.handleChange('email')}
               />
             </div>
-            <div className="form-group">
+
+            <div className="mt-4 form-group">
+              <label>パスワード（半角英数字で8 ~ 40文字）</label>
               <Input
                 type="password"
-                style={inputStyle}
-                placeholder="パスワード"
+                // style={inputStyle}
+                placeholder="パスワードを入力"
                 value={this.state.password}
                 onChange={this.handleChange('password')}
               />
             </div>
           </section>
 
-          <section className="text-center" onClick={this.signin.bind(this)}>
-            <ColorButton className="w-75 mt-2" color="#2b6db2">
+          <section
+            className="mt-5 text-center"
+            onClick={this.signin.bind(this)}
+          >
+            <ColorButton
+              className="w-75"
+              color="#2b6db2"
+              disabled={!email || password.length < Rule.PASS_MIN_LENGTH}
+            >
               ログインする
             </ColorButton>
           </section>
@@ -124,6 +137,7 @@ class Signin extends React.Component {
             text-decoration: underline;
           }
 
+          label,
           .alert {
             font-size: 12px;
           }
