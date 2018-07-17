b172452d0b358116c6af73b15fb129224cd752c9
Your Name <fkmks247@gmail.com>
2018-07-17 18:45:33 +0900

    home画面のアイコン間スペース開けた

--- react-web/components/templates/container/NavBar.js
+++ react-web/components/templates/container/NavBar.js
@@ -57,9 +57,9 @@ class NavBar extends React.Component {
             data-action={SiteCommon.SET_NAV_ICON_COLOR}
             data-path={`${PATH_MAP.NAV_ICON}`}
           >
-            <i className="fas fa-search mr-2 navIcon" />
+            <i className="fas fa-search mr-3 navIcon" />
             <NotificationIcon
-              className="mr-2 navIcon"
+              className="mr-3 navIcon"
               color={props.navIcon.color}
             />
 
