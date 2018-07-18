a1b45d86a0c9e055a74db1996f9401d1f90653e9
Your Name <fkmks247@gmail.com>
2018-07-18 19:16:23 +0900

    add commentId column to voice_log table

--- api-server/models/voice_log.js
+++ api-server/models/voice_log.js
@@ -4,7 +4,9 @@ module.exports = function(sequelize, DataTypes) {
     postId: DataTypes.INTEGER,
     voterId: DataTypes.INTEGER,
     // 0 - 4 : どの選択肢を選んだのか
-    choiceIndex: DataTypes.INTEGER
+    choiceIndex: DataTypes.INTEGER,
+    // 投票に紐づくコメント。
+    commentId: DataTypes.INTEGER
   })
   return VoiceLog
 }
