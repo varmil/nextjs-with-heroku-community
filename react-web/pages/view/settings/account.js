d3f0da0ca0ee39d0c4a0ea612283f72408bd4798
Your Name <fkmks247@gmail.com>
2018-08-03 19:31:02 +0900

    WIP implement user account setting

--- react-web/pages/view/settings/account.js
+++ react-web/pages/view/settings/account.js
@@ -2,29 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createAction } from 'redux-actions'
 import { Link, Router } from 'routes'
+import Input from 'reactstrap/lib/Input'
+import FormFeedback from 'reactstrap/lib/FormFeedback'
 import * as utilFiles from 'utils/files'
 import ProfileIconSelector from 'components/atoms/ProfileIconSelector'
 import BorderedTextHeader from 'components/organisms/site/BorderedTextHeader'
+import Rule from '/../shared/constants/Rule'
 
-const Item = props => (
-  <div
-    className={`row ${props.className || ''}`}
-    onClick={props.onClick || (() => {})}
-  >
-    <div className="col-9">{props.children}</div>
-    <div className="col-2">
-      <i className="fas fa-chevron-right" />
-    </div>
+const NULL_FEEDBACK = '1文字以上入力してください。'
 
-    <style jsx>{`
-      .row {
-        font-size: 17px;
-        font-weight: bold;
-        color: #4b4b4b;
-      }
-    `}</style>
-  </div>
-)
+const inputStyle = {
+  borderRadius: 10
+}
 
 class Account extends React.Component {
   constructor(props) {
@@ -45,6 +34,12 @@ class Account extends React.Component {
     this.state = state
   }
 
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    })
+  }
+
   onDrop(files) {
     if (files.length > 1) {
       files = files.slice(0, 1)
@@ -54,7 +49,8 @@ class Account extends React.Component {
 
   render() {
     const props = this.props
-    const { files } = this.state
+    const { files, nickname, description, email, password } = this.state
+    const passLength = password.length
     return (
       <React.Fragment>
         <BorderedTextHeader text="アカウント設定" />
@@ -69,20 +65,69 @@ class Account extends React.Component {
               />
             </div>
           </section>
+
+          <section className="mt-0">
+            <label className="mb-1">ユーザ名</label>
+            <Input
+              type="text"
+              style={inputStyle}
+              value={nickname}
+              onChange={this.handleChange('nickname')}
+              invalid={nickname === ''}
+            />
+            <FormFeedback>{NULL_FEEDBACK}</FormFeedback>
+          </section>
+
+          <section className="mt-3">
+            <label className="mb-1">自己紹介</label>
+            <Input
+              type="textarea"
+              rows={3}
+              style={inputStyle}
+              value={description}
+              onChange={this.handleChange('description')}
+            />
+          </section>
+
+          <section className="mt-3">
+            <label className="mb-1">メールアドレス</label>
+            <Input
+              type="text"
+              style={inputStyle}
+              value={email}
+              onChange={this.handleChange('email')}
+              invalid={email === ''}
+            />
+            <FormFeedback>{NULL_FEEDBACK}</FormFeedback>
+          </section>
+
+          <section className="mt-3">
+            <label className="mb-1">パスワード</label>
+            <Input
+              type="text"
+              style={inputStyle}
+              value={password}
+              onChange={this.handleChange('password')}
+              invalid={passLength > 0 && passLength < Rule.PASS_MIN_LENGTH}
+            />
+            <FormFeedback>
+              {Rule.PASS_MIN_LENGTH}文字以上入力してください
+            </FormFeedback>
+          </section>
         </div>
 
         <style jsx>{`
-          .header span {
-            font-size: 20px;
-            font-weight: bold;
-          }
-
-          .avatar i {
-            font-size: 22px;
+          .container {
+            padding-left: 45px;
+            padding-right: 45px;
           }
 
           .contentWrap {
             font-size: 16px;
+          }
+
+          label {
+            font-size: 12px;
           }
         `}</style>
       </React.Fragment>
