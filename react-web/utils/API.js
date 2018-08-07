715f702ad3d3615979b11dac85de511aae59efa2
Your Name <fkmks247@gmail.com>
2018-08-07 11:38:45 +0900

    modify API url

--- react-web/utils/API.js
+++ react-web/utils/API.js
@@ -5,24 +5,25 @@ const HTTP_SCHEMA = 'http://'
 const HTTPS_SCHEMA = 'https://'
 const SERVER_LOCALHOST = 'localhost'
 const DEV_SERVER = env.DEV_API_SERVER_URL || 'localhost'
-const PRODUCTION_SERVER = env.PROD_API_SERVER_URL || '35.200.117.204'
+const PRODUCTION_SERVER = env.PROD_API_SERVER_URL || 'apicommune.dayone.jp'
 const PORT = 5000
 
 export const getBaseDomain = () => {
   // server
   const isServer = typeof window === 'undefined'
-  if (isServer) return `${HTTP_SCHEMA}${SERVER_LOCALHOST}`
+  if (isServer) return `${HTTP_SCHEMA}${SERVER_LOCALHOST}:${PORT}`
 
   // browser
   if (process.env.NODE_ENV === 'production') {
+    // use domain and SSL
     return `${HTTPS_SCHEMA}${PRODUCTION_SERVER}`
   } else {
-    return `${HTTP_SCHEMA}${DEV_SERVER}`
+    return `${HTTP_SCHEMA}${DEV_SERVER}:${PORT}`
   }
 }
 
 const getBaseURL = () => {
-  return `${getBaseDomain()}:${PORT}`
+  return getBaseDomain()
 }
 
 // serverではシングルトンなので注意
