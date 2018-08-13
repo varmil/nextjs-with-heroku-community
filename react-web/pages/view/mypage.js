0805b11214329fc21bfdb706fc6d7175c18ce15f
Your Name <fkmks247@gmail.com>
2018-08-13 15:41:11 +0900

    WIP implement react-slick

--- react-web/pages/view/mypage.js
+++ react-web/pages/view/mypage.js
@@ -1,13 +1,78 @@
 import React from 'react'
+import Head from 'next/head'
 import range from 'lodash/range'
 import { connect } from 'react-redux'
 import { createAction } from 'redux-actions'
+import Slider from 'react-slick'
 import IconButton from '@material-ui/core/IconButton'
 import { Link, Router } from 'routes'
 import Avatar from 'components/atoms/Avatar'
 import MypageContents from 'components/templates/edit_view_shared/MypageContents'
 import { AppMypage } from 'constants/ActionTypes'
 import URL from 'constants/URL'
+
+class SimpleSlider extends React.Component {
+  render() {
+    const { className } = this.props
+    var settings = {
+      dots: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+    return (
+      <React.Fragment>
+        <Slider className={`${className || ''}`} {...settings}>
+          <div>
+            <div className="row justify-content-center">
+              {range(6).map(i => (
+                <div key={i} className="badge col-3 py-2">
+                  <img src="/static/stub/badges/001.png" />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h3>2</h3>
+          </div>
+
+          <div>
+            <h3>3</h3>
+          </div>
+
+          <div>
+            <h3>4</h3>
+          </div>
+        </Slider>
+
+        <style global jsx>{`
+          .slick-dots {
+            top: -5px;
+            bottom: initial;
+          }
+
+          .slick-dots li {
+            margin: 0 0px;
+          }
+        `}</style>
+
+        <style jsx>{`
+          .row {
+            width: 270px;
+            margin: 0 auto;
+          }
+
+          .badge img {
+            width: 52px;
+            height: 73px;
+            object-fit: cover;
+          }
+        `}</style>
+      </React.Fragment>
+    )
+  }
+}
 
 const iconButtonStyle = {
   position: 'absolute',
@@ -24,6 +89,21 @@ class Mypage extends React.Component {
     const props = this.props
     return (
       <React.Fragment>
+        <Head>
+          <title>Mypage</title>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+        </Head>
+
         <div className="container">
           <section className="avatar mt-4 text-center">
             <Avatar src={props.user.iconPath} size={60} />
@@ -63,12 +143,12 @@ class Mypage extends React.Component {
             </div>
           </section> */}
 
-          <section className="badges mt-2 row justify-content-center">
-            {range(6).map(i => (
-              <div key={i} className="badge col-3 py-2">
-                <img src="/static/stub/badges/001.png" />
-              </div>
-            ))}
+          <section className="badges position-relative mt-3">
+            <Link route={'/view/badge'}>
+              <div className="badgeLink">詳しく見る</div>
+            </Link>
+
+            <SimpleSlider className="pt-4 mb-4" />
           </section>
         </div>
 
@@ -101,6 +181,14 @@ class Mypage extends React.Component {
             font-size: 11px;
           }
 
+          .badgeLink {
+            color: #909090;
+            position: absolute;
+            top: 5px;
+            right: 10%;
+            font-size: 11px;
+          }
+
           .desc {
             font-weight: bold;
             font-size: 14px;
@@ -121,17 +209,6 @@ class Mypage extends React.Component {
 
           .act .entity:not(:last-child) {
             border-right: 1px solid black;
-          }
-
-          .badges {
-            width: 270px;
-            margin: 0 auto;
-          }
-
-          .badge img {
-            width: 52px;
-            height: 73px;
-            object-fit: cover;
           }
         `}</style>
       </React.Fragment>
