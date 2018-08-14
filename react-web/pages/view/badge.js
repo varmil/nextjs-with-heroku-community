e924710f1116a3504764dcc53991d2657d80c514
Your Name <fkmks247@gmail.com>
2018-08-14 12:05:33 +0900

    implement view/badge

--- react-web/pages/view/badge.js
+++ react-web/pages/view/badge.js
@@ -1,27 +1,41 @@
 import React from 'react'
 import Head from 'next/head'
+import find from 'lodash/find'
 import map from 'lodash/map'
 import range from 'lodash/range'
 import { connect } from 'react-redux'
 import { createAction } from 'redux-actions'
-import Slider from 'react-slick'
 import IconButton from '@material-ui/core/IconButton'
 import { Link, Router } from 'routes'
 import { SimpleSlider } from 'components/molecules/BadgeSlick'
-import { AppMypage } from 'constants/ActionTypes'
+import { AppBadge } from 'constants/ActionTypes'
 import URL from 'constants/URL'
 import { Badge } from '/../shared/constants/Badge'
 
 const BadgeSlick = props => (
   <div>
     {map(Badge, (v, badgeType) => {
+      const myBadgeStatus = find(props.badges, { badgeType: +badgeType })
+      console.log('@@', myBadgeStatus, badgeType, props.badges)
+
       return (
         <div key={v.imgname} className="row justify-content-center text-center">
-          {range(props.fromLevel, props.toLevel + 1).map(level => (
-            <div key={v.imgname + level} className="badge col-4 py-2">
-              <img src={`/static/img/badge/png/${v.imgname}0${level}.png`} />
-            </div>
-          ))}
+          {range(props.fromLevel, props.toLevel + 1).map(level => {
+            const myLevel = myBadgeStatus ? myBadgeStatus.level : 0
+            // 次のレベルは影表示
+            const shadow = level === myLevel + 1 ? 'shadow-' : ''
+            // 2つ以上上のレベルははてな
+            const imgSrc =
+              level > myLevel + 1
+                ? 'hatena.png'
+                : `${v.imgname}${shadow}0${level}.png`
+
+            return (
+              <div key={v.imgname + level} className="badge col-4 py-2">
+                <img src={`/static/img/badge/png/${imgSrc}`} />
+              </div>
+            )
+          })}
         </div>
       )
     })}
@@ -47,9 +61,11 @@ const iconButtonStyle = {
 }
 
 class BadgePage extends React.Component {
-  // static async getInitialProps({ ctx }) {
-  //   return {}
-  // }
+  static async getInitialProps({ ctx }) {
+    const { dispatch } = ctx.store
+    dispatch(createAction(AppBadge.FETCH_LIST_REQUEST)())
+    return {}
+  }
 
   render() {
     const props = this.props
@@ -80,8 +96,8 @@ class BadgePage extends React.Component {
 
           <section className="badges position-relative mt-1">
             <SimpleSlider className="pt-4 mb-4">
-              <BadgeSlick fromLevel={1} toLevel={3} />
-              <BadgeSlick fromLevel={4} toLevel={6} />
+              <BadgeSlick badges={badges} fromLevel={1} toLevel={3} />
+              <BadgeSlick badges={badges} fromLevel={4} toLevel={6} />
             </SimpleSlider>
           </section>
         </div>
